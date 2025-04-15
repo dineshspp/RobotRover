37b63e90177caf0d2aa3b8cd03baec8ca88e9d71
Dinesh <dinesh_spp@hotmail.com>
2025-04-15 10:27:47 +1000

    cleand up and updated readme

--- main.js
+++ main.js
@@ -2,23 +2,7 @@ const { DIRECTION_INDEX } = require("./direction");
 const Robot = require("./robot").Robot;
 const fs = require('fs')
 
-
-
 fs.readFile('commands.txt', 'utf8', (err, text) => {
     let robot = new Robot(0, 0, DIRECTION_INDEX.NORTH);
     robot.processCommands(text.split("\n"));
 });
-
-// let robot = new Robot(0, 0, DIRECTION_INDEX.NORTH);
-// fetch("commands.txt")
-//   .then((res) => res.text())
-//   .then((text) => {
-//     console.log(text);
-//     robot.processCommands(text.split("\n"));
-//    })
-//   .catch((e) => console.error(e));
-
-// // let commands = ["PLACE 0,0,NORTH", "MOVE", "REPORT"];
-// // robot.processCommands(commands);
-// // commands = ["PLACE 0,0,NORTH", "LEFT", "REPORT"];
-// // robot.processCommands(commands);
---
+++ readme.md
@@ -0,0 +1,21 @@
+1. Checkout the project from the repository
+2. Updated the commands.txt file with the commands you want to execute
+3. Run the main.js file
+4. The robot will move according to the commands
+
+## Commands
+
+### PLACE x,y,direction
+Places the robot at the given coordinates and direction
+
+### MOVE
+Moves the robot in the current direction
+
+### LEFT
+Rotates the robot to the left
+
+### RIGHT
+Rotates the robot to the right
+
+### REPORT
+Reports the current position and direction of the robot in the console
